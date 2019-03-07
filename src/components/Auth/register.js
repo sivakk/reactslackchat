628e8268d8b1cc7ce055800393707276621483ae
sivakk <ksiva1355@gmail.com>
2019-03-07 11:28:13 +0530

    router

---
+++ src/components/Auth/register.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Register extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (
+
+            <div>Register</div>
+          );
+    }
+}
+ 
+export default Register;
